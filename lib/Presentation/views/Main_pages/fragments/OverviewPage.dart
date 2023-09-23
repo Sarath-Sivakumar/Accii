ce919daabcc0d806527add4353060f3ce22dc325
Sarath-Sivakumar <sarathputhenvila@gmail.com>
2023-09-23 12:42:09 +0530

    first commit

---
+++ lib/Presentation/views/Main_pages/fragments/OverviewPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class Overview_page extends StatelessWidget {
+  const Overview_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "OverView Page here"
+        ),
+      )
+    );
+  }
+}
